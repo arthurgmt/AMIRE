8a06afc75b06f4b87a9b09089828ce53be44cdbf
bboudou <berengere94@gmail.com>
2023-11-02 16:06:16 +0100

    update bdd

--- AMIRE/script.sql
+++ AMIRE/script.sql
@@ -2,20 +2,20 @@ CREATE DATABASE Amire;
 USE Amire;
 CREATE TABLE Utilisateur(
     ID INT PRIMARY KEY,
-    Nom VARCHAR(255),
-    Prenom VARCHAR(255),
-    Mail VARCHAR(255),
-    MotDePasse VARCHAR(255),
-    Role VARCHAR(50)
+    Nom VARCHAR(255) NOT NULL ,
+    Prenom VARCHAR(255) NOT NULL,
+    Mail VARCHAR(255) NOT NULL,
+    MotDePasse VARCHAR(255) NOT NULL,
+    Role VARCHAR(50)  NOT NULL
 );
 CREATE TABLE Enseignant (
     ID INT PRIMARY KEY,
-    UtilisateurID INT,
+    UtilisateurID INT NOT NULL,
     Experience INT,
-    Telephone VARCHAR(20),
+    Telephone VARCHAR(20) NOT NULL,
     SiteWeb VARCHAR(255),
     NiveauxSouhaites VARCHAR(255),
-    Disponibilites DATE,
+    Disponibilites DATE NOT NULL,
     TypeContrat VARCHAR(50),
     TitresAcademiques TEXT,
     Evaluations TEXT,
@@ -24,48 +24,48 @@ CREATE TABLE Enseignant (
 );
 CREATE TABLE Ecole (
     ID INT PRIMARY KEY,
-    RaisonSociale VARCHAR(255),
-    Adresse TEXT,
+    RaisonSociale VARCHAR(255) NOT NULL,
+    Adresse TEXT NOT NULL,
     SiteWeb VARCHAR(255),
     Contact VARCHAR(255)
 );
 CREATE TABLE Besoin (
     ID INT PRIMARY KEY,
-    EcoleID INT,
-    Periode VARCHAR(255),
+    EcoleID INT NOT NULL,
+    Periode VARCHAR(255) NOT NULL,
     Remarques TEXT,
-    Exigences TEXT,
+    Exigences TEXT NOT NULL,
     FOREIGN KEY (EcoleID) REFERENCES Ecole(ID)
 );
 CREATE TABLE Decision (
     ID INT PRIMARY KEY,
-    Statut VARCHAR(50),
-    DateDecision DATE,
+    Statut VARCHAR(50) NOT NULL,
+    DateDecision DATE NOT NULL,
     Commentaires TEXT
 );
 CREATE TABLE Candidature (
      ID INT AUTO_INCREMENT PRIMARY KEY,
-     EnseignantID INT,
-     BesoinID INT,
-     DecisionID INT,
+     EnseignantID INT NOT NULL,
+     BesoinID INT NOT NULL,
+     DecisionID INT NOT NULL,
      FOREIGN KEY (EnseignantID) REFERENCES Enseignant(ID),
      FOREIGN KEY (BesoinID) REFERENCES Besoin(ID),
      FOREIGN KEY (DecisionID) REFERENCES Decision(ID)
 );
 CREATE TABLE Competence (
     ID INT PRIMARY KEY,
-    NomCompetence VARCHAR(255)
+    NomCompetence VARCHAR(255) NOT NULL
 );
 CREATE TABLE BesoinCompetence (
-    BesoinID INT,
-    CompetenceID INT,
+    BesoinID INT NOT NULL,
+    CompetenceID INT NOT NULL,
     PRIMARY KEY (BesoinID, CompetenceID),
     FOREIGN KEY (BesoinID) REFERENCES Besoin(ID),
     FOREIGN KEY (CompetenceID) REFERENCES Competence(ID)
 );
 CREATE TABLE EnseignantCompetence (
-    EnseignantID INT,
-    CompetenceID INT,
+    EnseignantID INT NOT NULL,
+    CompetenceID INT NOT NULL,
     PRIMARY KEY (EnseignantID, CompetenceID),
     FOREIGN KEY (EnseignantID) REFERENCES Enseignant(ID),
     FOREIGN KEY (CompetenceID) REFERENCES Competence(ID)
