31d1a9eb0be942519e61bccd4582e643eed8eea6
arthurgmt <arthur.guillaumot@gmail.com>
2023-11-03 16:58:11 +0100

    add user DAO

--- AMIRE/src/main/java/dao/UtilisateurDAO.java
+++ AMIRE/src/main/java/dao/UtilisateurDAO.java
@@ -1,7 +1,7 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -20,7 +20,11 @@ public class UtilisateurDAO {
                 Utilisateur utilisateur = new Utilisateur();
                 utilisateur.setID(rs.getInt("ID"));
                 utilisateur.setNom(rs.getString("Nom"));
-                // ... set autres attributs
+                utilisateur.setPrenom(rs.getString("Prenom"));
+                utilisateur.setMail(rs.getString("Mail"));
+                utilisateur.setMotDePasse(rs.getString("MotDePasse"));
+                utilisateur.setRole(rs.getString("Role"));
+
                 utilisateurs.add(utilisateur);
             }
         } catch (SQLException e) {
@@ -29,5 +33,78 @@ public class UtilisateurDAO {
         return utilisateurs;
     }
 
-    // ... autres méthodes pour INSERT, UPDATE, DELETE, etc.
+    public void addUser(Utilisateur utilisateur) {
+        String SQL = "INSERT INTO Utilisateur(Nom, Prenom, Mail, MotDePasse, Role) VALUES(?, ?, ?, ?, ?)";
+        try (Connection conn = DatabaseConfiguration.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(SQL)) {
+
+            stmt.setString(1, utilisateur.getNom());
+            stmt.setString(2, utilisateur.getPrenom());
+            stmt.setString(3, utilisateur.getMail());
+            stmt.setString(4, utilisateur.getMotDePasse());
+            stmt.setString(5, utilisateur.getRole());
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateUser(Utilisateur utilisateur) {
+        String SQL = "UPDATE Utilisateur SET Nom=?, Prenom=?, Mail=?, MotDePasse=?, Role=? WHERE ID=?";
+        try (Connection conn = DatabaseConfiguration.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(SQL)) {
+
+            stmt.setString(1, utilisateur.getNom());
+            stmt.setString(2, utilisateur.getPrenom());
+            stmt.setString(3, utilisateur.getMail());
+            stmt.setString(4, utilisateur.getMotDePasse());
+            stmt.setString(5, utilisateur.getRole());
+            stmt.setInt(6, utilisateur.getID());
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteUser(int id) {
+        String SQL = "DELETE FROM Utilisateur WHERE ID=?";
+        try (Connection conn = DatabaseConfiguration.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(SQL)) {
+
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Utilisateur login(String mail, String password) {
+        String SQL = "SELECT * FROM Utilisateur WHERE Mail=? AND MotDePasse=?";
+        try (Connection conn = DatabaseConfiguration.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(SQL)) {
+
+            stmt.setString(1, mail);
+            stmt.setString(2, password);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                Utilisateur utilisateur = new Utilisateur();
+                utilisateur.setID(rs.getInt("ID"));
+                utilisateur.setNom(rs.getString("Nom"));
+                utilisateur.setPrenom(rs.getString("Prenom"));
+                utilisateur.setMail(rs.getString("Mail"));
+                utilisateur.setMotDePasse(rs.getString("MotDePasse"));
+                utilisateur.setRole(rs.getString("Role"));
+                return utilisateur;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // Pour le logout, généralement c'est géré au niveau de la session de l'application, pas au niveau du DAO
 }
